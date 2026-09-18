1c955f39ce1b0cd02228a5b43c358d42b7802137
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-09-19 00:07:53 +0530

    Convert windowed mode to borderless fullscreen.

--- src/d3d9.c
+++ src/d3d9.c
@@ -29,46 +29,47 @@ HRESULT WINAPI CreateDevice(PVOID this, UINT adapter, D3DDEVTYPE type, HWND wnd,
 {
     static BOOL s_flag = {};
 
-    D3DPRESENT_PARAMETERS d3dpp = *params;
-    DWORD style = d3dpp.Windowed ? WS_OVERLAPPEDWINDOW : WS_POPUP;
-
-    if (IsWindowVisible(wnd))
-        style |= WS_VISIBLE;
-
-    d3dpp.Windowed = TRUE;
-    d3dpp.FullScreen_RefreshRateInHz = 0;
-    d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
-    d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
-
-    flags |= D3DCREATE_NOWINDOWCHANGES;
-    HRESULT hr = g_CreateDevice(this, adapter, type, wnd, flags, &d3dpp, device);
-
-    if (SUCCEEDED(hr) && !s_flag)
+    if (params->Windowed)
     {
-        s_flag = TRUE;
+        D3DPRESENT_PARAMETERS d3dpp = *params;
 
-        MH_CreateHook((*device)->lpVtbl->Reset, Reset, (PVOID)&g_Reset);
-        MH_CreateHook((*device)->lpVtbl->Present, Present, (PVOID)&g_Present);
+        d3dpp.Windowed = TRUE;
+        d3dpp.FullScreen_RefreshRateInHz = 0;
+        d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+        d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
 
-        MH_QueueEnableHook((*device)->lpVtbl->Reset);
-        MH_QueueEnableHook((*device)->lpVtbl->Present);
-        MH_ApplyQueued();
+        flags |= D3DCREATE_NOWINDOWCHANGES;
+        HRESULT hr = g_CreateDevice(this, adapter, type, wnd, flags, &d3dpp, device);
 
-        PWSTR atom = MAKEINTATOM(RegisterClassW(&(WNDCLASSW){
-            .lpszClassName = L"PRESENT",
-            .lpfnWndProc = DefWindowProcW,
-            .hCursor = LoadCursorW(NULL, IDC_ARROW),
-            .hbrBackground = GetStockObject(BLACK_BRUSH),
-        }));
+        if (SUCCEEDED(hr) && !s_flag)
+        {
+            s_flag = TRUE;
 
-        g_Wnd = CreateWindowExW(WS_EX_LEFT, atom, NULL, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, wnd, NULL, NULL, NULL);
-        g_WndProc = (PVOID)SetWindowLongW(wnd, GWLP_WNDPROC, (LONG_PTR)WndProc);
+            MH_CreateHook((*device)->lpVtbl->Reset, Reset, (PVOID)&g_Reset);
+            MH_CreateHook((*device)->lpVtbl->Present, Present, (PVOID)&g_Present);
 
-        SetWindowLongW(wnd, GWL_STYLE, style);
-        SetWindowLongW(wnd, GWL_EXSTYLE, WS_EX_APPWINDOW);
+            MH_QueueEnableHook((*device)->lpVtbl->Reset);
+            MH_QueueEnableHook((*device)->lpVtbl->Present);
+            MH_ApplyQueued();
 
-        SetWindowPos(wnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+            PWSTR atom = MAKEINTATOM(RegisterClassW(&(WNDCLASSW){
+                .lpszClassName = L"PRESENT",
+                .lpfnWndProc = DefWindowProcW,
+                .hCursor = LoadCursorW(NULL, IDC_ARROW),
+                .hbrBackground = GetStockObject(BLACK_BRUSH),
+            }));
+
+            g_Wnd = CreateWindowExW(WS_EX_LEFT, atom, NULL, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, wnd, NULL, NULL, NULL);
+            g_WndProc = (PVOID)SetWindowLongW(wnd, GWLP_WNDPROC, (LONG_PTR)WndProc);
+
+            SetWindowLongW(wnd, GWL_EXSTYLE, WS_EX_APPWINDOW);
+            SetWindowLongW(wnd, GWL_STYLE, WS_POPUP | (IsWindowVisible(wnd) * WS_VISIBLE));
+
+            SetWindowPos(wnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+        }
+
+        return hr;
     }
 
-    return hr;
+    return E_FAIL;
 }
