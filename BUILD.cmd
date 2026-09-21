6a84ae5ce49fd6db02f26d7ab4a975c96f7c2290
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-09-21 12:06:29 +0530

    Do not use the standard library or CRT.

--- BUILD.cmd
+++ BUILD.cmd
@@ -12,7 +12,7 @@ md "obj"
 i686-w64-mingw32-windres.exe -i "res.rc" -o "obj\res.o"
 
 i686-w64-mingw32-gcc.exe ^
--Oz -shared -e "DllMain" ^
+-nostdlib -s -Oz -shared -e "DllMain" ^
 -DUNICODE -DINITGUID -DWIN32_LEAN_AND_MEAN -DWINVER=NTDDI_WIN10 ^
 -Wl,--kill-at,--gc-sections,--exclude-all-symbols -Wno-dll-attribute-on-redeclaration ^
 "main.c" "obj\res.o" -lkernel32 -luser32 -lgdi32 -ld3d9 -lshlwapi -ldwmapi -o "bin/dinput8.dll"
