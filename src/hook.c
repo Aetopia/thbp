790c697543b36f027a797e6039938a071380cea0
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-09-21 11:58:30 +0530

    Fix relative addresses being incorrectly calculated.

--- src/hook.c
+++ src/hook.c
@@ -10,20 +10,20 @@ PVOID CreateHook(PVOID target, PVOID detour)
         PVOID addr;
     } jmp = {.code = 0xE9};
 
-    PVOID ptr = VirtualAlloc(NULL, sizeof jmp * 2, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-    jmp.addr = (PVOID)((target + sizeof jmp) - (ptr - sizeof jmp * 2));
+    PVOID og = VirtualAlloc(NULL, sizeof jmp * 2, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    jmp.addr = (PVOID)((target + sizeof jmp) - (og + sizeof jmp * 2));
 
-    __movsb(ptr, target, sizeof jmp);
-    __movsb(ptr + sizeof jmp, (PVOID)&jmp, sizeof jmp);
+    __movsb(og, target, sizeof jmp);
+    __movsb(og + sizeof jmp, (PVOID)&jmp, sizeof jmp);
 
     DWORD flags = {};
     VirtualProtect(target, sizeof jmp, PAGE_EXECUTE_READWRITE, &flags);
 
-    jmp.addr = (PVOID)(detour - (ptr + sizeof jmp));
+    jmp.addr = (PVOID)(detour - (target + sizeof jmp));
     __movsb(target, (PVOID)&jmp, sizeof jmp);
 
     VirtualProtect(target, sizeof jmp, flags, &(DWORD){});
-    FlushInstructionCache(GetCurrentProcess(), NULL, 0);
+    FlushInstructionCache(GetCurrentProcess(), target, sizeof jmp);
 
-    return ptr;
+    return og;
 }
