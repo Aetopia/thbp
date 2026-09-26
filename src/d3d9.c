33cabe825c9d91167974384a4bcc2300de5517e5
Aetopia <41850963+Aetopia@users.noreply.github.com>
2026-09-26 10:06:05 +0530

    Modify present parameters to specify the presentation window.

--- src/d3d9.c
+++ src/d3d9.c
@@ -4,16 +4,11 @@
 #include <d3d9.h>
 
 HRESULT WINAPI (*g_Reset)(PVOID, PVOID) = {};
-HRESULT WINAPI (*g_Present)(PVOID, PVOID, PVOID, HWND, PVOID) = {};
 HRESULT WINAPI (*g_CreateDevice)(PVOID, UINT, D3DDEVTYPE, HWND, DWORD, PVOID, PVOID) = {};
-
-HRESULT WINAPI Present(PVOID this, PVOID src, PVOID dst, HWND wnd, PVOID rgn)
-{
-    return g_Present(this, NULL, NULL, g_Wnd, NULL);
-}
 
 HRESULT WINAPI Reset(PVOID this, D3DPRESENT_PARAMETERS *params)
 {
+    params->hDeviceWindow = g_Wnd;
     params->PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
     return params->Windowed ? g_Reset(this, params) : E_FAIL;
 }
@@ -21,27 +16,25 @@ HRESULT WINAPI Reset(PVOID this, D3DPRESENT_PARAMETERS *params)
 HRESULT WINAPI CreateDevice(PVOID this, UINT adapter, D3DDEVTYPE type, HWND wnd, DWORD flags,
                             D3DPRESENT_PARAMETERS *params, LPDIRECT3DDEVICE9 *device)
 {
-    static BOOL s_flag = {};
+    if (!g_Wnd)
+        g_Wnd = CreateWindowExW(WS_EX_LEFT, L" ", NULL, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, wnd, NULL, NULL, NULL);
 
-    flags |= D3DCREATE_NOWINDOWCHANGES;
+    if (!g_WndProc)
+    {
+        g_WndProc = (PVOID)SetWindowLongW(wnd, GWLP_WNDPROC, (LONG_PTR)WndProc);
+
+        SetWindowLongW(wnd, GWL_EXSTYLE, WS_EX_APPWINDOW);
+        SetWindowLongW(wnd, GWL_STYLE, WS_POPUP | WS_CLIPCHILDREN | (IsWindowVisible(wnd) * WS_VISIBLE));
+
+        SetWindowPos(wnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+    }
+
+    params->hDeviceWindow = g_Wnd;
     params->PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;
     HRESULT hr = params->Windowed ? g_CreateDevice(this, adapter, type, wnd, flags, params, device) : E_FAIL;
 
-    if (SUCCEEDED(hr) && !s_flag)
-    {
-        s_flag = TRUE;
-
-        g_WndProc = (PVOID)SetWindowLongW(wnd, GWLP_WNDPROC, (LONG_PTR)WndProc);
-        g_Wnd = CreateWindowExW(WS_EX_LEFT, L" ", NULL, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, wnd, NULL, NULL, NULL);
-
-        SetWindowLongW(wnd, GWL_EXSTYLE, WS_EX_APPWINDOW);
-        SetWindowLongW(wnd, GWL_STYLE, WS_CLIPCHILDREN | WS_POPUP | (IsWindowVisible(wnd) * WS_VISIBLE));
-
-        SetWindowPos(wnd, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
-
+    if (SUCCEEDED(hr) && !g_Reset)
         g_Reset = CreateHook((*device)->lpVtbl->Reset, Reset);
-        g_Present = CreateHook((*device)->lpVtbl->Present, Present);
-    }
 
     return hr;
 }
